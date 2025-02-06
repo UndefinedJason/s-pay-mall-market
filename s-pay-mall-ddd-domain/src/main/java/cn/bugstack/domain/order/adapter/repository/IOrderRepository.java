64715b8ba4240fc8f757de287122ee0bce06c9c1
fuzhengwei <184172133@qq.com>
2025-02-06 14:00:32 +0800

    feat: 小型支付商城对接拼团

---
+++ s-pay-mall-ddd-domain/src/main/java/cn/bugstack/domain/order/adapter/repository/IOrderRepository.java
@@ -0,0 +1,24 @@
+package cn.bugstack.domain.order.adapter.repository;
+
+import cn.bugstack.domain.order.model.aggregate.CreateOrderAggregate;
+import cn.bugstack.domain.order.model.entity.OrderEntity;
+import cn.bugstack.domain.order.model.entity.PayOrderEntity;
+import cn.bugstack.domain.order.model.entity.ShopCartEntity;
+
+import java.util.List;
+
+public interface IOrderRepository {
+    void doSaveOrder(CreateOrderAggregate orderAggregate);
+
+    OrderEntity queryUnPayOrder(ShopCartEntity shopCartEntity);
+
+    void updateOrderPayInfo(PayOrderEntity payOrderEntity);
+
+    void changeOrderPaySuccess(String orderId);
+
+    List<String> queryNoPayNotifyOrder();
+
+    List<String> queryTimeoutCloseOrderList();
+
+    boolean changeOrderClose(String orderId);
+}
---
+++ s-pay-mall-ddd-infrastructure/src/main/java/cn/bugstack/infrastructure/dao/IOrderDao.java
@@ -0,0 +1,25 @@
+package cn.bugstack.infrastructure.dao;
+
+import cn.bugstack.infrastructure.dao.po.PayOrder;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IOrderDao {
+
+    void insert(PayOrder payOrder);
+
+    PayOrder queryUnPayOrder(PayOrder payOrder);
+
+    void updateOrderPayInfo(PayOrder payOrder);
+
+    void changeOrderPaySuccess(PayOrder payOrderReq);
+
+    List<String> queryNoPayNotifyOrder();
+
+    List<String> queryTimeoutCloseOrderList();
+
+    boolean changeOrderClose();
+
+}
