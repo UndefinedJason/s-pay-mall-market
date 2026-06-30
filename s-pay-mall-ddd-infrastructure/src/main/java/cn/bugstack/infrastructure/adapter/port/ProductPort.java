90fd0644a5d3137f3455498fc2aef0dd7111f76b
jason <355232518@qq.com>
2026-06-30 16:48:24 +0800

    重新来

--- s-pay-mall-ddd-infrastructure/src/main/java/cn/bugstack/infrastructure/adapter/port/ProductPort.java
+++ s-pay-mall-ddd-infrastructure/src/main/java/cn/bugstack/infrastructure/adapter/port/ProductPort.java
@@ -84,7 +84,7 @@ public class ProductPort implements IProductPort {
                     .build();
         } catch (Exception e) {
             log.error("营销锁单失败{}", userId, e);
-            return null;
+            throw new RuntimeException("营销锁单失败", e);
         }
     }
 
